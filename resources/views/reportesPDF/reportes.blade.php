036c8a2d93b5c6a5a54d5b74b90cc33ea4a81c41
JoseMejia <antonio.mejia22@itca.edu.sv>
2023-06-12 11:13:54 -0600

    cambios

--- resources/views/reportesPDF/reportes.blade.php
+++ resources/views/reportesPDF/reportes.blade.php
@@ -19,33 +19,35 @@
 
     {{--Botón desplegable para reportes PDF--}}
     <div class="container">
-      <div class="row">
-          {{--Reporte de todos los empleados--}}
-          <div class="col-lg-6 mt-4">
-              <div class="card h-100">
-                  <div class="row">
-                    <div class="col d-flex align-items-center justify-content-center">
-                      <img src="{{ asset('/css/img/empleados.png')}}" class="card-img img-fuid" style="width: 60%;">
-                    </div>
-                    <div class="col">
-                      <div class="card-body">
-                        <h5 class="card-title">Generar reporte TODOS los empleados</h5>
-                        <p class="card-text">Click en el botón para mostrar o descargar el reporte.</p>
-                        <div class="dropdown mt-4">
-                              {{--Boton reporte PDF--}}
-                              <button class="btn btn btn-success dropdown-toggle text-white" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                              Reporte PDF empleados
-                              </button>
-                              <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
-                              <li><a class="dropdown-item text-white" href="/reporte-empleadosTodos">Mostrar | Todos los empleados</a></li>
-                              <li><a class="dropdown-item text-white" href="/reporte-empleadosDescargar">Descargar | Todos los empleados</a></li>
-                              </ul>
-                          </div>
-                      </div>
-                    </div>
-                  </div>
+    <div class="row">
+      {{--Reporte de todos los empleados--}}
+        <div class="col-lg-6 mt-4">
+          <div class="card h-100">
+            <div class="row">
+              <div class="col d-flex align-items-center justify-content-center">
+                <img src="{{ asset('/css/img/empleados.png')}}" class="card-img img-fuid" style="width: 60%;">
               </div>
+
+              <div class="col">
+                <div class="card-body">
+                  <h5 class="card-title">Generar reporte TODOS los empleados</h5>
+                  <p class="card-text">Click en el botón para mostrar o descargar el reporte.</p>
+                  <div class="dropdown mt-4">
+                        {{--Boton reporte PDF--}}
+                        <button class="btn btn btn-success dropdown-toggle text-white" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Reporte PDF empleados
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item text-white" href="/reporte-empleadosTodos">Mostrar | Todos los empleados</a></li>
+                        <li><a class="dropdown-item text-white" href="/reporte-empleadosDescargar">Descargar | Todos los empleados</a></li>
+                        </ul>
+                    </div>
+                </div>
+              </div>
+            </div>
           </div>
+      </div>
+    </div>
 
           {{--Generar reporte según áreas de empleados--}}
           <div class="col-lg-6 mt-4">
